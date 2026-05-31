d398d2cf7ed5ccb96e3fa1fcc661918bdbfe29d4
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2026-05-31 18:10:18 +0800

    Update build.sh

--- dist/debian/build.sh
+++ dist/debian/build.sh
@@ -12,11 +12,10 @@ r2 -qv
 if [ $? != 0 ]; then
 	# git clone --depth=1 git@github.com:radareorg/radare2 r2 || exit 1
     apt-get update
-	apt-get -y install xz-utils dpkg-dev pkg-config wget binutils git g++ make pkg-config flex bison unzip patch
-	wget https://github.com/radareorg/radare2/archive/master.zip
-	unzip master.zip
-	mv radare2-master r2
-	( cd r2 && sys/debian.sh ) # make -C r2/dist/debian
+	apt-get -y install xz-utils git dpkg-dev pkg-config wget binutils git g++ make pkg-config flex bison unzip patch
+	git clone https://github.com/radareorg/radare2 r2
+	git submodules update --init --recursive
+	( cd r2 && bash sys/debian.sh ) # make -C r2/dist/debian
 	dpkg -i r2/dist/debian/*/*.deb
 fi
 [ -z "${DESTDIR}" ] && DESTDIR="/work/dist/debian/root"
