d46cd6ac36c98a94e2918b8e05b4f024f58ac872
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2026-05-31 17:56:18 +0800

    Update build.sh

--- dist/debian/build.sh
+++ dist/debian/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eu
+
 type fakeroot > /dev/null 2>&1
 if [ $? = 0 ]; then
 FAKEROOT=fakeroot
@@ -11,13 +13,13 @@ r2 -qv
 
 if [ $? != 0 ]; then
 	# git clone --depth=1 git@github.com:radareorg/radare2 r2 || exit 1
-	wget -c https://github.com/radareorg/radare2/archive/master.zip
-	sudo apt-get update
-	sudo apt-get -y install git g++ make pkg-config flex bison unzip patch
+    apt-get update
+	apt-get -y install wget binutils git g++ make pkg-config flex bison unzip patch
+	https://github.com/radareorg/radare2/archive/master.zip
 	unzip master.zip
 	mv radare2-master r2
 	( cd r2 && sys/debian.sh ) # make -C r2/dist/debian
-	sudo dpkg -i r2/dist/debian/*/*.deb
+	dpkg -i r2/dist/debian/*/*.deb
 fi
 [ -z "${DESTDIR}" ] && DESTDIR="/work/dist/debian/root"
 
